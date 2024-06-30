b1026cbabf20ccf5c8cd98a09d84e0c2708db372
Your Name <hrayhansyah@gmail.com>
2024-06-30 21:10:30 +0700

    benerin loading

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -5,6 +5,7 @@ import { Star, Truck } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useCart } from "@/hooks/cartContext";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function DetailProduct() {
 	const { addToCart } = useCart();
@@ -28,14 +29,6 @@ export default function DetailProduct() {
 		}
 	}, [slug]);
 
-	if (loading) {
-		return <div>Loading...</div>;
-	}
-
-	if (!product) {
-		return <div>Product not found</div>;
-	}
-
 	const handleAddToCart = () => {
 		addToCart({
 			id: product._id,
@@ -52,50 +45,69 @@ export default function DetailProduct() {
 			<div className='bg-white'>
 				<div className='mx-auto max-w-screen-xl px-4 md:px-8'>
 					<div className='grid gap-8 md:grid-cols-2'>
-						<ImageGallery images={product.images} />
-
-						<div className='md:py-8 mt-12'>
-							<div className='mb-2 md:mb-3'>
-								{/* <span className='mb-0.5 inline-block text-gray-500'>
-									{product.category}
-								</span> */}
-								<h2 className='text-2xl font-bold text-gray-800 lg:text-3xl'>
-									{product.name}
-								</h2>
-							</div>
-
-							<div className='mb-4'>
-								<div className='flex items-end gap-2'>
-									<span className='text-xl font-bold text-gray-800 md:text-2xl'>
-										Rp{product.price.toLocaleString("id-ID")}
-									</span>
+						{loading ? (
+							<div className='animate-pulse flex space-x-4'>
+								<div className='w-full'>
+									<Skeleton className='h-96 w-full rounded-lg' />
 								</div>
-
-								<div className='mt-4'>
-									{/* ukuran */}
-									<button className=' bg-black mr-2 duration-200 flex place-items-center justify-center rounded-full w-12 h-12 cursor-pointer hover:bg-cusblack hover:text-white'></button>
+								<div className='w-full'>
+									<div className='space-y-4'>
+										<Skeleton className='h-6 w-1/2' />
+										<Skeleton className='h-6 w-3/4' />
+										<Skeleton className='h-6 w-1/3' />
+										<Skeleton className='h-6 w-2/3' />
+										<Skeleton className='h-6 w-1/2' />
+										<Skeleton className='h-6 w-4/5' />
+									</div>
 								</div>
 							</div>
+						) : (
+							<>
+								<ImageGallery images={product.images} />
 
-							<div className='mb-6 flex items-center gap-2 text-gray-500'>
-								<Truck className='h-6 w-6' />
-								<span className='text-sm'>2-4 Day Shipping</span>
-							</div>
-							<div className='flex gap-2.5'>
-								<button
-									className='w-full h-12 rounded-md bg-zinc-500 text-2xl font-medium text-white'
-									onClick={() => handleAddToCart()}
-								>
-									add to cart
-								</button>
-								<button className='w-full rounded-md bg-orange-500 text-2xl font-medium text-white'>
-									buy now
-								</button>
-							</div>
-							<p className='mt-12 text-base tracking-wide text-gray-500'>
-								{product.description}
-							</p>
-						</div>
+								<div className='md:py-8 mt-12'>
+									<div className='mb-2 md:mb-3'>
+										<h2 className='text-2xl font-bold text-gray-800 lg:text-3xl'>
+											{product.name}
+										</h2>
+									</div>
+
+									<div className='mb-4'>
+										<div className='flex items-end gap-2'>
+											<span className='text-xl font-bold text-gray-800 md:text-2xl'>
+												Rp{product.price.toLocaleString("id-ID")}
+											</span>
+										</div>
+
+										<div className='mt-4'>
+											{/* ukuran */}
+											<button className=' bg-black mr-2 duration-200 flex place-items-center justify-center rounded-full w-12 h-12 cursor-pointer hover:bg-cusblack hover:text-white'></button>
+										</div>
+									</div>
+
+									<div className='mb-6 flex items-center gap-2 text-gray-500'>
+										<Truck className='h-6 w-6' />
+										<span className='text-sm'>2-4 Day Shipping</span>
+									</div>
+
+									<div className='flex gap-2.5'>
+										<button
+											className='w-full h-12 rounded-md bg-zinc-500 text-2xl font-medium text-white'
+											onClick={() => handleAddToCart()}
+										>
+											add to cart
+										</button>
+										<button className='w-full rounded-md bg-orange-500 text-2xl font-medium text-white'>
+											buy now
+										</button>
+									</div>
+
+									<p className='mt-12 text-base tracking-wide text-gray-500'>
+										{product.description}
+									</p>
+								</div>
+							</>
+						)}
 					</div>
 				</div>
 			</div>
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,24 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { products } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function ProductsAll() {
 	const [products, setProducts] = useState([]);
+	const [loading, setLoading] = useState(true);
 
 	const dataProduct = async () => {
 		try {
 			const response = await fetch("/api/product/getproduct");
 			const data = await response.json();
 			setProducts(data);
+			setLoading(false);
 		} catch (error) {
 			console.error("Error fetching data:", error);
+			setLoading(false);
 		}
 	};
 
 	useEffect(() => {
 		dataProduct();
 	}, []);
+
 	return (
 		<>
 			<div className='bg-white '>
@@ -30,40 +34,56 @@ export default function ProductsAll() {
 					</div>
 
 					<div className='mt-8 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
-						{products.map((product) => (
-							<div key={product._id} className='group relative'>
-								<div className='aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80'>
-									<Link href={`/product/${product.slug}`}>
-										{product.images.map((image) => (
-											<Image
-												key={image}
-												src={image}
-												alt='Product image'
-												className='h-full w-full object-cover object-center lg:h-full lg:w-full'
-												width={300}
-												height={300}
-											/>
-										))}
-									</Link>
-								</div>
+						{loading
+							? Array.from({ length: 8 }, (_, index) => (
+									<div key={index} className='group relative'>
+										<div className='aspect-square w-full overflow-hidden rounded-md bg-gray-200 animate-pulse group-hover:opacity-75 lg:h-80'>
+											<Skeleton className='h-full w-full object-cover object-center lg:h-full lg:w-full' />
+										</div>
 
-								<div className='mt-4 flex justify-between'>
-									<div>
-										<h3 className='text-sm text-gray-700'>
-											<Link href={`/product/${product.slug}`}>
-												{product.name}
-											</Link>
-										</h3>
-										<p className='mt-1 text-sm text-gray-500'>
-											{product.categoryName}
-										</p>
+										<div className='mt-4 flex justify-between'>
+											<div>
+												<Skeleton className='h-6 w-3/4' />
+												<Skeleton className='h-4 w-1/2 mt-1' />
+											</div>
+											<Skeleton className='h-6 w-1/4' />
+										</div>
 									</div>
-									<p className='text-sm font-medium text-gray-900'>
-										Rp{product.price}
-									</p>
-								</div>
-							</div>
-						))}
+							  ))
+							: products.map((product) => (
+									<div key={product._id} className='group relative'>
+										<div className='aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80'>
+											<Link href={`/product/${product.slug}`}>
+												{product.images.map((image, index) => (
+													<Image
+														key={index}
+														src={image}
+														alt='Product image'
+														className='h-full w-full object-cover object-center lg:h-full lg:w-full'
+														width={300}
+														height={300}
+													/>
+												))}
+											</Link>
+										</div>
+
+										<div className='mt-4 flex justify-between'>
+											<div>
+												<h3 className='text-sm text-gray-700'>
+													<Link href={`/product/${product.slug}`}>
+														{product.name}
+													</Link>
+												</h3>
+												<p className='mt-1 text-sm text-gray-500'>
+													{product.categoryName}
+												</p>
+											</div>
+											<p className='text-sm font-medium text-gray-900'>
+												Rp{product.price}
+											</p>
+										</div>
+									</div>
+							  ))}
 					</div>
 				</div>
 			</div>
